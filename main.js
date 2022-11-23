edb3d722174d73c3c9ef252f979afa24841aef04
bubinq <emilbumov@gmail.com>
2022-11-23 18:20:40 +0200

    Added cell splitting feature

--- main.js
+++ main.js
@@ -3,8 +3,11 @@ const ctx = canvas.getContext("2d");
 
 const WIDTH = (canvas.width = window.innerWidth);
 const HEIGHT = (canvas.height = window.innerHeight);
-const MAX_FOOD = 100;
-const MASS_RATIO = 50;
+const MAX_FOOD = 300;
+const MASS_RATIO = 10;
+const MAX_CELLS = 8;
+const INITIAL_RADIUS = 20;
+const MAX_SPLIT_TIME = 20;
 const mouseCoordinates = {
   x: 0,
   y: 0,
@@ -32,6 +35,8 @@ class Player {
       x: 0,
       y: 0,
     };
+    this.cells = [];
+    this.lastSplit = new Date().getTime();
   }
 
   update() {
@@ -39,6 +44,7 @@ class Player {
     this.x += this.velocity.x;
     this.y += this.velocity.y;
     this.checkDistanceToMouse();
+    this.manageCellsMovement();
   }
 
   draw() {
@@ -57,6 +63,92 @@ class Player {
       this.velocity.x *= 0.95;
       this.velocity.y *= 0.95;
     }
+  }
+
+  manageCellsMovement() {
+    this.cells.forEach((cell) => {
+      const dist =
+        Math.hypot(this.y - cell.y, this.x - cell.x) -
+        cell.radius -
+        this.radius;
+      const deg = Math.atan2(
+        mouseCoordinates.y - cell.y + cell.radius / 2,
+        mouseCoordinates.x - cell.x + cell.radius / 2
+      );
+      const velocity = {
+        x: Math.cos(deg),
+        y: Math.sin(deg),
+      };
+      cell.velocity.x = velocity.x * (dist * 0.0112);
+      cell.velocity.y = velocity.y * (dist * 0.0112);
+    });
+    for (let i = 0; i < this.cells.length; i++) {
+      if (this.lastSplit < new Date().getTime() - 1000 * MAX_SPLIT_TIME) {
+        const dist =
+          Math.hypot(this.cells[i].x - this.x, this.cells[i].y - this.y) -
+          this.radius -
+          this.cells[i].radius;
+        if (dist < -10) {
+          this.radius += this.cells[i].radius;
+          this.cells.splice(i, 1);
+          break;
+        }
+      }
+      for (let j = 1; j < this.cells.length; j++) {
+        if (this.cells[j] !== undefined) {
+          const dist = Math.hypot(
+            this.cells[i].y - this.cells[j].y,
+            this.cells[i].x - this.cells[j].x
+          );
+          if (
+            dist !== 0 &&
+            dist - this.cells[i].radius - this.cells[j].radius < 1
+          ) {
+            if (this.lastSplit > new Date().getTime() - 1000 * MAX_SPLIT_TIME) {
+              if (this.cells[i].x > this.cells[j].x) {
+                this.cells[j].x--;
+              } else if (this.cells[i].x < this.cells[j].x) {
+                this.cells[j].x++;
+              }
+              if (this.cells[i].y > this.cells[j].y) {
+                this.cells[j].y--;
+              } else if (this.cells[i].y < this.cells[j].y) {
+                this.cells[j].y++;
+              }
+            } else {
+              if (dist - this.cells[i].radius - this.cells[j].radius < -10) {
+                this.cells[i].radius += this.cells[j].radius;
+                this.cells.splice(j, 1);
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+}
+
+class Cell {
+  constructor(x, y, radius, color) {
+    this.x = x;
+    this.y = y;
+    this.radius = radius;
+    this.color = color;
+    this.velocity = {
+      x: 0,
+      y: 0,
+    };
+  }
+  update() {
+    this.draw();
+    this.x += this.velocity.x;
+    this.y += this.velocity.y;
+  }
+  draw() {
+    ctx.beginPath();
+    ctx.fillStyle = this.color;
+    ctx.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false);
+    ctx.fill();
   }
 }
 
@@ -81,9 +173,20 @@ class Food {
   }
   checkIsEaten() {
     const distance = Math.hypot(this.y - player.y, this.x - player.x);
-    if (distance - player.radius < 5) {
+    if (distance - player.radius < 1) {
+      player.radius += this.mass;
       this.isEaten = true;
     }
+    player.cells.forEach((cell) => {
+      let cellDist =
+        Math.hypot(this.y - cell.y, this.x - cell.x) -
+        cell.radius -
+        this.radius;
+      if (cellDist < 1) {
+        cell.radius += this.mass;
+        this.isEaten = true;
+      }
+    });
   }
 }
 
@@ -94,12 +197,23 @@ let player = new Player(
   bestCandidate.x,
   bestCandidate.y,
   "Gosho",
-  20,
+  INITIAL_RADIUS,
   colors[Math.floor(Math.random() * colors.length)]
 );
 window.addEventListener("mousemove", movePlayer);
-// ADD EVENTLISTENER FOR KEYPRESS === SPACE
-// TAKE PLAYER RADIUS AND DIVIDE IT BY 2 ALSO MAKE ANOTHER COPY OF THE PLAYER WITH THE SAME SIZE
+window.addEventListener("keydown", splitCell);
+
+function splitCell(ev) {
+  const key = ev.code;
+  if (key === "Space") {
+    if (player.radius > INITIAL_RADIUS * 2 && player.cells.length < MAX_CELLS) {
+      const mass = Math.round(player.radius / 2);
+      player.radius -= mass;
+      player.cells.push(new Cell(player.x, player.y, mass, player.color));
+      player.lastSplit = new Date().getTime();
+    }
+  }
+}
 
 function movePlayer(ev) {
   const x = ev.clientX;
@@ -121,7 +235,6 @@ function movePlayer(ev) {
   player.velocity.x = velocity.x / (player.radius * 0.05);
   player.velocity.y = velocity.y / (player.radius * 0.05);
 }
-
 
 function generateFood() {
   for (let i = 0; i < MAX_FOOD; i++) {
@@ -186,7 +299,7 @@ function getFarthestPos(points, radius) {
       maxDistance = minDistance;
     }
   }
-  return bestCandidate
+  return bestCandidate;
 }
 
 function animate() {
@@ -195,9 +308,11 @@ function animate() {
   food.forEach((foodPiece, idx) => {
     foodPiece.update();
     if (foodPiece.isEaten) {
-      player.radius += foodPiece.mass;
       food.splice(idx, 1);
     }
+  });
+  player.cells.forEach((cell) => {
+    cell.update();
   });
   requestAnimationFrame(animate);
 }
